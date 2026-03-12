d0e1cf030960ad23c63886451d10a77da4484c81
pk910 <philipp@pk910.de>
2026-03-13 00:58:26 +0100

    bump dynamic-ssz

--- benchmarks/dynamicssz-codegen/generate.go
+++ benchmarks/dynamicssz-codegen/generate.go
@@ -1,3 +1,3 @@
 package dynamicssz
 
-//go:generate go run github.com/pk910/dynamic-ssz/dynssz-gen@v1.2.0 -package . -types SignedBeaconBlock,BeaconBlock,BeaconState -output gen_ssz.go -legacy -with-streaming
+//go:generate go run github.com/pk910/dynamic-ssz/dynssz-gen@v1.2.2 -package . -types SignedBeaconBlock,BeaconBlock,BeaconState -output gen_ssz.go -legacy -with-streaming
